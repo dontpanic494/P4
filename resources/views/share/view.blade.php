5b51c41970888e07687cbfb55881e2bc7131512a
John Lincoln <jlincoln88@gmail.com>
2016-05-09 01:10:52 -0400

    recipe view styling (initial)

--- resources/views/share/view.blade.php
+++ resources/views/share/view.blade.php
@@ -9,10 +9,17 @@
 @section('content')
 
 	<h1>{{ $recipe->recipe_name }}</h1>
-	<img src=' {{ $recipe->recipe_image }}' class= 'img-responsive' alt= 'Image for {{ $recipe->recipe_name }}'>
+	<h3>{{ $recipe->recipe_description }}</h3>
 
+	<div>
+	<img src=' {{ $recipe->recipe_image }}' class= 'img-responsive' alt= 'Image for {{ $recipe->recipe_name }}'>
+	</div>
+
+	<div>
 	@foreach($ingredients_for_this_recipe as $ingredient => $ingredient_id)
 		<p>{{ $ingredient_id }}</p>
 	@endforeach
+	</div>
+
 
 @stop
