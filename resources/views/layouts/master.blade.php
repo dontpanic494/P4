9902011a191090c7c35f2e617ef8794da3d66264
John Lincoln <jlincoln88@gmail.com>
2016-05-09 01:06:52 -0400

    corrected really silly error in master template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,8 +10,8 @@
 		<meta charset='utf-8'>
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	    <link rel="stylesheet" type="text/css" href="css/bootstrapUnited.css">
-	    <link rel="stylesheet" type="text/css" href="css/main.css">
+	    <link rel="stylesheet" type="text/css" href="/css/bootstrapUnited.css">
+	    <link rel="stylesheet" type="text/css" href="/css/main.css">
 	    <link rel="shortcut icon" href="/favicon.ico" /> 
 
 		@yield('head')
