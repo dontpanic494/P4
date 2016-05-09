a83851907f73cc8fbd6639fc35e5e03ad11c057f
John Lincoln <jlincoln88@gmail.com>
2016-05-09 00:50:48 -0400

    fixed home page link

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,7 @@
 <div class="jumbotron">
   <h1>Welcome to Fridge Buffet!</h1>
   <p>This site is currently...like...super under constuction. But feel free to click around, I guess. You'll love it if empty pages are your thing.</p>
-  <p><a class="btn btn-primary btn-lg">Let's do it</a></p>
+  <p><a href="/share"class="btn btn-primary btn-lg">Let's do it</a></p>
 </div>
 
 
