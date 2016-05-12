c46ef83fbe387227548205c8b0dc9fa11c2a93ad
John Lincoln <jlincoln88@gmail.com>
2016-05-11 22:48:19 -0400

    syling

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,7 @@
 <div class="jumbotron">
   <h1>Welcome to Fridge Buffet!</h1>
   <p>A user created database of awesome recipes!</p>
-  <p><a href="/share"class="btn btn-primary btn-lg">Check it out!</a></p>
+  <p><a href="/share" class="btn btn-primary btn-lg">Check it out!</a></p>
 </div>
 
 
